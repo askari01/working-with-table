2420b4874e4c401d3145803bfa6e6977769b9c07
Farrukh Askari <farrukh.askari01@gmail.com>
2016-01-25 10:52:38 -0800

    working with tables
    using table views

--- working with table/ViewController.swift
+++ working with table/ViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var cities = ["Karachi", "Lahore", "Islamabad", "Peshawar", "Quetta"]
+    // MARK: UITablePropertise
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cities.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        cell.textLabel?.text = cities[indexPath.row]
+        return cell
+    }
 
 }
 
